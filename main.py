01aa70d83742b1eeaa50da2363e7248fd10d6165
kashlya <154353601+kashlya@users.noreply.github.com>
2024-02-20 22:33:57 +0100

    фигура_3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ a = int(input("число a: "))
 b = int(input("число b: "))
 suma = 0
 dilniki = a
+max_num = 0
 if a < b:
     for i in range(a, b + 1):
         ch = 0
@@ -11,6 +12,7 @@ if a < b:
         if ch > suma:
             dilniki = i
             suma = ch
-    print(dilniki, suma)
+            max_num = i
+    print(dilniki, suma, max_num)
 else:
     print("false")
